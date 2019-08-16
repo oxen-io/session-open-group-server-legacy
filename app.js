a8e98efd9e485a853aa8826c1da83f80ff7dac06
Beaudan Brown <beau@loki.network>
2019-08-16 17:12:09 +1000

    Initial generating token, encrypting and sending to client

--- app.js
+++ app.js
@@ -1,71 +1,55 @@
-const express    = require('express');
-const request    = require('request');
-const bodyParser = require('body-parser');
-const Cookies    = require('cookies');
-const multer     = require('multer');
+const express         = require('express');
+const request         = require('request');
+const bodyParser      = require('body-parser');
+const Cookies         = require('cookies');
+const multer          = require('multer');
+const sodium          = require('libsodium-wrappers');
+const bb              = require('bytebuffer');
 
 const app = express();
 const router = express.Router();
 
 const storage = multer.memoryStorage();
 
-const NONCE_LEN = 30;
 const tempDB = {};
 
-const generateNonce = () => {
-  let nonce = '';
+const generateToken = (pubKey) => {
+  // Temp function
+  const TOKEN_LEN = 24;
+  let token = '';
   const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  for (let i = 0; i < NONCE_LEN; i++) {
-    nonce += possible.charAt(Math.floor(Math.random() * possible.length));
+  for (let i = 0; i < TOKEN_LEN; i++) {
+    token += possible.charAt(Math.floor(Math.random() * possible.length));
   }
-  return nonce;
+  saveToken(pubKey, token);
+  return token;
 }
 
-const saveToken = async (pubKey, token) => {
+const saveToken = (pubKey, token) => {
   // Temp function, hit db
   return;
 }
 
-const generateToken = async (pubKey) => {
-  // Temp function, async to hit db
-  let token = '';
-  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  for (let i = 0; i < NONCE_LEN; i++) {
-    token += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  await saveToken(pubKey, token);
-  return token;
-}
+const getEncryptedToken = (clientPubKey) => {
+  const { publicKey, privateKey } = sodium.crypto_box_keypair();
+  const serverPubKey64 = bb.wrap(publicKey).toString('base64');
 
-const getNonceTimer = (pubKey) => {
-  setTimeout(() => {
-    // 2 minute nonce timeout for temp db
-    if (tempDB[pubKey]) {
-      delete tempDB[pubKey];
-    }
-  }, 12000);
-}
+  const nonce = sodium.randombytes_buf(sodium.crypto_secretbox_NONCEBYTES);
+  const nonce64 = bb.wrap(nonce).toString('base64');
 
-const getOrCreateNonce = (pubKey) => {
-  if (!tempDB[pubKey]) {
-    tempDB[pubKey] = {
-      nonce: generateNonce(),
-      timer: getNonceTimer(),
-    }
-    console.log(`New nonce: ${tempDB[pubKey].nonce}`);
-  } else {
-    clearTimeout(tempDB[pubKey].timer);
-    tempDB[pubKey].timer = getNonceTimer();
-    console.log(`Second nonce: ${tempDB[pubKey].nonce}`);
-  }
-  return tempDB[pubKey].nonce;
-}
+  const token = generateToken(clientPubKey);
+  const tokenData = sodium.from_string(token);
 
-const validSignature = (pubKey, signature) => {
-  const nonce = getOrCreateNonce(pubKey);
-  // Check sig
-  console.log(`Signature valid: ${signature}`);
-  return true;
+  const clientPubKeyData = sodium.from_hex(clientPubKey).slice(1); // Remove messenger leading 05
+
+  const cipherText = sodium.crypto_box_easy(tokenData, nonce, clientPubKeyData, privateKey);
+  const cipherText64 = bb.wrap(cipherText).toString('base64');
+
+  return {
+    cipherText64,
+    nonce64,
+    serverPubKey64,
+  };
 }
 
 /** need this for POST parsing */
@@ -93,38 +77,15 @@ app.all('/*', (req, res, next) => {
   next();
 });
 
-app.post('/loki/v1/startRegistration', (req, res) => {
+app.post('/loki/v1/getToken', (req, res) => {
   const { pubKey } = req.body;
   if (!pubKey) {
-    console.log('startRegistration arguments missing');
+    console.log('getToken pubKey missing');
     res.status(422).end('PubKey missing');
-  }
-  const nonce = getOrCreateNonce(pubKey);
-  res.status(200).type('application/json').end(JSON.stringify({ nonce }));
-});
-
-app.post('/loki/v1/submitRegistration', async (req, res) => {
-  const { pubKey, signature } = req.body;
-  if (!pubKey || !signature) {
-    console.log('submitRegistration arguments missing');
-    res.status(422).end('Arguments missing');
     return;
   }
-  if (!validSignature(pubKey, signature)) {
-    const nonce = generateNonce(pubKey);
-    res.status(401).type('application/json').end(JSON.stringify({ nonce }));
-    return;
-  }
-  const token = await generateToken(pubKey);
-  res.status(200).type('application/json').end(JSON.stringify({ token }));
-});
-
-app.use((req, res, next) => {
-  req.cookies = new Cookies(req, res);
-  res.path = req.path;
-  console.log(res.path);
-  console.log(`Request received: ${res.path}`);
-  next();
+  const responseBody = getEncryptedToken(pubKey);
+  res.status(200).type('application/json').end(JSON.stringify(responseBody));
 });
 
 app.listen(8081);
