719d9992158e19877869f05b4c8ef038bf98770c
Ryan Tharp <neuro@interx.net>
2019-08-18 23:48:59 +0000

    implement user/token find/create

--- app.js
+++ app.js
@@ -13,21 +13,42 @@ const storage = multer.memoryStorage();
 
 const tempDB = {};
 
-const generateToken = (pubKey) => {
-  // Temp function
-  const TOKEN_LEN = 24;
-  let token = '';
-  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  for (let i = 0; i < TOKEN_LEN; i++) {
-    token += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  saveToken(pubKey, token);
-  return token;
+const cache = require('../sapphire-platform-server/dataaccess.caminte.js');
+const ADN_SCOPES = 'basic stream write_post follow messages update_profile files export';
+
+const findOrCreateUser = (pubKey, cb) => {
+  cache.getUserID(pubKey, (user, err) => {
+    if (err) {
+      cb(err, null);
+      return;
+    }
+    if (user === null) {
+      // create user
+      // "password" (2nd) parameter is not saved/used
+      cache.addUser(pubKey, '', (newUser, err2) => {
+        cb(err2, newUser);
+      })
+    } else {
+      // we have this user
+      cb(null, user);
+    }
+  })
 }
 
-const saveToken = (pubKey, token) => {
-  // Temp function, hit db
-  return;
+const findOrCreateToken = (pubKey, cb) => {
+  findOrCreateUser(pubKey, (userErr, user) => {
+    if (userErr) {
+      return cb(userErr, user);
+    }
+    // create token
+    cache.createOrFindUserToken(user.id, 'loki', ADN_SCOPES, (usertoken, tokenErr) => {
+      if (tokenErr) {
+        return cb(tokenErr, usertoken);
+      }
+      // we don't need to validation application because there is only one application
+      cb(tokenErr, usertoken);
+    })
+  })
 }
 
 const getEncryptedToken = (clientPubKey) => {
@@ -37,19 +58,27 @@ const getEncryptedToken = (clientPubKey) => {
   const nonce = sodium.randombytes_buf(sodium.crypto_secretbox_NONCEBYTES);
   const nonce64 = bb.wrap(nonce).toString('base64');
 
-  const token = generateToken(clientPubKey);
-  const tokenData = sodium.from_string(token);
+  return new Promise(function(resolve, reject) {
+    findOrCreateToken(clientPubKey, (err, userToken) => {
+      if (err) {
+        return reject(err);
+      }
+      const token = userToken.token;
+      const tokenData = sodium.from_string(token);
 
-  const clientPubKeyData = sodium.from_hex(clientPubKey).slice(1); // Remove messenger leading 05
+      const clientPubKeyData = sodium.from_hex(clientPubKey).slice(1); // Remove messenger leading 05
 
-  const cipherText = sodium.crypto_box_easy(tokenData, nonce, clientPubKeyData, privateKey);
-  const cipherText64 = bb.wrap(cipherText).toString('base64');
+      const cipherText = sodium.crypto_box_easy(tokenData, nonce, clientPubKeyData, privateKey);
+      const cipherText64 = bb.wrap(cipherText).toString('base64');
 
-  return {
-    cipherText64,
-    nonce64,
-    serverPubKey64,
-  };
+      // could return userToken.userid
+      resolve({
+        cipherText64,
+        nonce64,
+        serverPubKey64,
+      });
+    })
+  })
 }
 
 /** need this for POST parsing */
@@ -59,6 +88,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.all('/*', (req, res, next) => {
+  console.log('got request', req.path)
   res.start=new Date().getTime();
   origin = req.get('Origin') || '*';
   res.set('Access-Control-Allow-Origin', origin);
@@ -84,8 +114,12 @@ app.post('/loki/v1/getToken', (req, res) => {
     res.status(422).end('PubKey missing');
     return;
   }
-  const responseBody = getEncryptedToken(pubKey);
-  res.status(200).type('application/json').end(JSON.stringify(responseBody));
+  getEncryptedToken(pubKey).then(keyInfo => {
+    res.status(200).type('application/json').end(JSON.stringify(keyInfo));
+  }).catch(err => {
+    res.status(500).end(err.toString());
+    return;
+  })
 });
 
 app.listen(8081);
