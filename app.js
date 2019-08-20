ae78e58524e1c8bb367a74d2adacf9d886b87b8d
Beaudan Brown <beau@loki.network>
2019-08-20 14:08:28 +1000

    Move to 2 step auth

--- app.js
+++ app.js
@@ -16,69 +16,114 @@ const tempDB = {};
 const cache = require('../sapphire-platform-server/dataaccess.caminte.js');
 const ADN_SCOPES = 'basic stream write_post follow messages update_profile files export';
 
-const findOrCreateUser = (pubKey, cb) => {
-  cache.getUserID(pubKey, (user, err) => {
-    if (err) {
-      cb(err, null);
-      return;
+const getTokenTimer = (pubKey, token) => {
+  return setTimeout(() => {
+    // 2 minute token timeout for temp db
+    tempDB[pubKey] = tempDB[pubKey].filter(entry => {
+      entry.token !== token;
+    });
+    if (tempDB[pubKey].length === 0) {
+      delete tempDB[pubKey];
     }
-    if (user === null) {
-      // create user
-      // "password" (2nd) parameter is not saved/used
-      cache.addUser(pubKey, '', (newUser, err2) => {
-        cb(err2, newUser);
+  }, 120000);
+}
+
+const findOrCreateToken = async (pubKey) => {
+  return new Promise((res, rej) => {
+    findOrCreateUser(pubKey)
+      .then(user => {
+        cache.createOrFindUserToken(user.id, 'loki', ADN_SCOPES, (usertoken, tokenErr) => {
+          if (tokenErr) {
+            return rej(tokenErr);
+          }
+          // we don't need to validation application because there is only one application
+          res(usertoken);
+        })
       })
-    } else {
-      // we have this user
-      cb(null, user);
-    }
+      .catch(e => {
+        rej(e);
+      });
   })
 }
 
-const findOrCreateToken = (pubKey, cb) => {
-  findOrCreateUser(pubKey, (userErr, user) => {
-    if (userErr) {
-      return cb(userErr, user);
-    }
-    // create token
-    cache.createOrFindUserToken(user.id, 'loki', ADN_SCOPES, (usertoken, tokenErr) => {
-      if (tokenErr) {
-        return cb(tokenErr, usertoken);
+const findOrCreateUser = async (pubKey) => {
+  return new Promise((res, rej) => {
+    cache.getUserID(pubKey, (user, err) => {
+      if (err) {
+        rej(err);
+        return;
       }
-      // we don't need to validation application because there is only one application
-      cb(tokenErr, usertoken);
+      if (user === null) {
+        // create user
+        // "password" (2nd) parameter is not saved/used
+        cache.addUser(pubKey, '', (newUser, err2) => {
+          if (err2) {
+            rej(err2);
+          } else {
+            res(newUser);
+          }
+        })
+      } else {
+        // we have this user
+        res(user);
+      }
     })
   })
 }
 
-const getEncryptedToken = (clientPubKey) => {
+const confirmToken = async (pubKey, token) => {
+  if (!tempDB[pubKey]) {
+    return false;
+  }
+  // Check to ensure the token submitted has been sent to pubKey before
+  let tokenFound = false;
+  tempDB[pubKey] = tempDB[pubKey].filter(entry => {
+    const thisTokenFound = entry.token === token;
+    if (thisTokenFound) {
+      clearTimeout(entry.timer);
+      tokenFound = true;
+    }
+    return !thisTokenFound;
+  });
+  if (tempDB[pubKey].length === 0) {
+    delete tempDB[pubKey];
+  }
+  if (tokenFound) {
+    // TODO: Register token with platform?
+    return true;
+  }
+  return false;
+}
+
+const getChallenge = async (pubKey) => {
   const { publicKey, privateKey } = sodium.crypto_box_keypair();
   const serverPubKey64 = bb.wrap(publicKey).toString('base64');
+
+  const userToken = await findOrCreateToken(pubKey);
+  const token = userToken.token;
+  const tokenData = sodium.from_string(token);
 
   const nonce = sodium.randombytes_buf(sodium.crypto_secretbox_NONCEBYTES);
   const nonce64 = bb.wrap(nonce).toString('base64');
 
-  return new Promise(function(resolve, reject) {
-    findOrCreateToken(clientPubKey, (err, userToken) => {
-      if (err) {
-        return reject(err);
-      }
-      const token = userToken.token;
-      const tokenData = sodium.from_string(token);
+  const pubKeyData = sodium.from_hex(pubKey).slice(1); // Remove messenger leading 05
 
-      const clientPubKeyData = sodium.from_hex(clientPubKey).slice(1); // Remove messenger leading 05
+  const cipherText = sodium.crypto_box_easy(tokenData, nonce, pubKeyData, privateKey);
+  const cipherText64 = bb.wrap(cipherText).toString('base64');
 
-      const cipherText = sodium.crypto_box_easy(tokenData, nonce, clientPubKeyData, privateKey);
-      const cipherText64 = bb.wrap(cipherText).toString('base64');
+  if(!tempDB[pubKey]) {
+    tempDB[pubKey] = [];
+  }
+  tempDB[pubKey].push({
+    token,
+    timer: getTokenTimer(pubKey, token),
+  });
 
-      // could return userToken.userid
-      resolve({
-        cipherText64,
-        nonce64,
-        serverPubKey64,
-      });
-    })
-  })
+  return {
+    cipherText64,
+    nonce64,
+    serverPubKey64,
+  };
 }
 
 /** need this for POST parsing */
@@ -107,17 +152,45 @@ app.all('/*', (req, res, next) => {
   next();
 });
 
-app.post('/loki/v1/getToken', (req, res) => {
-  const { pubKey } = req.body;
+app.post('/loki/v1/submit_challenge', (req, res) => {
+  const { pubKey, token } = req.body;
   if (!pubKey) {
-    console.log('getToken pubKey missing');
-    res.status(422).end('PubKey missing');
+    console.log('submit_challenge pubKey missing');
+    res.status(422).type('application/json').end(JSON.stringify({
+      error: 'pubKey missing',
+    }));
     return;
   }
-  getEncryptedToken(pubKey).then(keyInfo => {
+  if (!token) {
+    console.log('submit_challenge token missing');
+    res.status(422).type('application/json').end(JSON.stringify({
+      error: 'token missing',
+    }));
+    return;
+  }
+  if (confirmToken(pubKey, token)) {
+    res.status(200).end();
+  } else {
+    res.status(500).end();
+  }
+});
+
+app.get('/loki/v1/get_challenge', (req, res) => {
+  const { pubKey } = req.query;
+  if (!pubKey) {
+    console.log('get_challenge pubKey missing');
+    res.status(422).type('application/json').end(JSON.stringify({
+      error: 'PubKey missing',
+    }));
+    return;
+  }
+  getChallenge(pubKey).then(keyInfo => {
     res.status(200).type('application/json').end(JSON.stringify(keyInfo));
   }).catch(err => {
-    res.status(500).end(err.toString());
+    console.log(`Error getting challenge: ${err}`);
+    res.status(500).type('application/json').end(JSON.stringify({
+      error: err.toString(),
+    }));
     return;
   })
 });
