2d7052e34485dfce268ecb97a05b92b72a29bb79
Ryan Tharp <neuro@interx.net>
2020-02-17 08:51:48 +0000

    pass overlay and start purge endpoints

--- dialects/token/dialect.loki_tokens.js
+++ dialects/token/dialect.loki_tokens.js
@@ -5,10 +5,16 @@ module.exports = (app, prefix) => {
   // set cache based on dispatcher object
   cache = app.dispatcher.cache;
   const utilties = overlay.setup(cache, app.dispatcher);
+  utilties.overlay = overlay;
   handlers.setup(utilties);
   const { storage, logic, config, dialect } = utilties;
 
   app.get(prefix + '/loki/v1/get_challenge', handlers.getChallengeHandler);
   app.post(prefix + '/loki/v1/submit_challenge', handlers.submitChallengeHandler);
   app.get(prefix + '/loki/v1/user_info', handlers.getTokenInfoHandler);
+
+  app.post(prefix + '/loki/v1/users/me/messages/purge', handlers.submitChallengeHandler);
+  app.post(prefix + '/loki/v1/users/me/files/purge', handlers.submitChallengeHandler);
+  app.post(prefix + '/loki/v1/users/me/channels/purge', handlers.submitChallengeHandler);
+
 }
