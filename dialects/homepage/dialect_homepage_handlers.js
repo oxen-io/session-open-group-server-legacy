f19b50dc08321cd8cebb5a104ceead86c7582748
Ryan Tharp <neuro@interx.net>
2020-05-21 00:08:57 +0000

    get page data

--- dialects/homepage/dialect_homepage_handlers.js
+++ dialects/homepage/dialect_homepage_handlers.js
@@ -10,6 +10,28 @@ const setup = (utilties) => {
 
 const homePageHandler = async (req, res) => {
   const version = pjson.version;
+  const disk_config = config.getDiskConfig()
+  if (!disk_config) disk_config = {}
+  if (!disk_config.api) disk_config.api = {}
+  // console.log('disk_config', disk_config);
+  cache.getAnnotations('channel', 1, function(note, err) {
+    if (err) console.error('error', err)
+    // console.log('note', JSON.stringify(note))
+
+    var value = JSON.parse(note[0].value)
+    // console.log('avatar', value.avatar)
+    var whitelistMode = config.inWhiteListMode();
+    console.log('whitelistMode', whitelistMode, disk_config)
+    res.render('index.ejs', {
+      version: version,
+      public_url: disk_config.api.public_url,
+      c1_name: value.name,
+      c1_logo: value.avatar,
+      c1_desc: value.description,
+      whitelistMode: whitelistMode,
+    });
+  })
+  /*
   res.end(`
 <html>
 <head>
@@ -31,6 +53,7 @@ const homePageHandler = async (req, res) => {
 </body>
 </html>
 `);
+  */
 };
 
 module.exports = {
