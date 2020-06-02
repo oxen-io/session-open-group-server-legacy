d51839d96ba3ac3fdbcc89af3ede9da9b1641a97
Ryan Tharp <neuro@interx.net>
2020-06-02 01:02:21 +0000

    test refactor

---
+++ test/tests/tokens/user_info.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+
+const lib = require('../lib');
+
+module.exports = (testInfo) => {
+  it('user info (non-mod)', async function() {
+    // test token endpoints
+    const result = await overlayApi.serverRequest('loki/v1/user_info');
+    //console.log('user user_info result', result)
+    assert.equal(200, result.statusCode);
+    assert.ok(result.response);
+    assert.ok(result.response.data);
+    // we're a freshly created user (hopefully)
+    assert.ok(!result.response.data.moderator_status);
+    assert.ok(result.response.data.user_id);
+    // FIXME: needs to be set inside testInfo I bet...
+    // or are we using node globals?
+    userid = result.response.data.user_id;
+  });
+}
