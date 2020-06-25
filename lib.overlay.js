3737855b4ab0452037e3da0a4bd27fa294047880
Ryan Tharp <neuro@interx.net>
2020-06-25 03:13:57 +0000

    only do set up things once, cleaning up logging

--- lib.overlay.js
+++ lib.overlay.js
@@ -17,14 +17,10 @@ const ADN_SCOPES = 'basic stream write_post follow messages update_profile files
 
 // Look for a config file
 const disk_config = config.getDiskConfig();
-storage.start(disk_config);
 
 preflight = false;
 
 const setup = (cache, dispatcher) => {
-  config.setup({ cache, storage });
-  logic.setup({ storage, cache, config });
-  dialect.setup({ dispatcher });
 
   // I guess we're do preflight checks here...
   const dataAccess = cache;
@@ -34,7 +30,9 @@ const setup = (cache, dispatcher) => {
     var defaultObj = {"name":"Your Public Chat","description":"Your public chat room","avatar":"images/group_default.png"};
     dataAccess.addAnnotation('channel', channelId, 'net.patter-app.settings', defaultObj, function(rec, err, meta) {
       if (err) console.error('err', err);
-      console.log('rec', rec, 'meta', meta);
+      if (!rec) {
+        console.warn('annotation', JSON.parse(JSON.stringify(rec)), 'meta', meta);
+      }
     });
   }
 
@@ -51,7 +49,7 @@ const setup = (cache, dispatcher) => {
         created_at: new Date
       }, async (msg, err) =>{
         if (err) console.error('addChannelMessage err', err);
-        console.log('addChannelMessage msg', JSON.parse(JSON.stringify(msg)));
+        // console.log('addChannelMessage msg', JSON.parse(JSON.stringify(msg)));
         if (msg.id) {
           var defaultObj = {
             timestamp: parseInt(Date.now() / 1000),
@@ -69,7 +67,8 @@ const setup = (cache, dispatcher) => {
           });
           defaultObj.sigver = 1;
           dataAccess.addAnnotation('message', msg.id, 'network.loki.messenger.publicChat', defaultObj, function(rec, err, meta) {
-            console.log('created message 1!', JSON.parse(JSON.stringify(rec)));
+            // , JSON.parse(JSON.stringify(rec))
+            console.log('created initial message for mobile');
             resolve(err, msg);
           });
         }
@@ -77,8 +76,15 @@ const setup = (cache, dispatcher) => {
     });
   }
 
+  // only do this once on startup...
   if (!preflight) {
     preflight = true
+
+    config.setup({ cache, storage });
+    logic.setup({ storage, cache, config });
+    dialect.setup({ dispatcher });
+    storage.start(disk_config);
+
     dataAccess.getChannel(1, {}, async (chnl, err, meta) => {
       if (err) console.error('channel 1 get err', err);
       if (chnl && chnl.id) {
@@ -112,15 +118,16 @@ const setup = (cache, dispatcher) => {
         }
         return;
       }
-      console.log('need to create channel 1!');
+      console.log('need to create initial channel');
       // FIXME: user token_helpers's findOrCreateUser?
       dataAccess.getUser(1, async (user, err2, meta2) => {
         if (err2) console.error('get user 1 err', err2);
         // if no user, create the user...
-        console.log('user', user);
+        // user === null when D.N.E.
+        // console.log('user', user);
         var privKey, pubKey;
         if (!user || !user.length) {
-          console.log('need to create user 1!');
+          console.log('need to create initial user');
           // block until this is complete
           user = await new Promise((resolve, rej) => {
             const ourKey = libsignal.curve.generateKeyPair();
@@ -139,12 +146,14 @@ const setup = (cache, dispatcher) => {
                 if (config.inWhiteListMode()) {
                   // add them to the white list...
                   const result = await logic.whitelistUserForServer(user.id);
-                  console.log('whitelisted', result)
+                  if (!result) {
+                    console.warn('could not whitelist!')
+                  }
                 }
                 // generate a token for server/tests
                 cache.createOrFindUserToken(user.id, 'messenger', ADN_SCOPES, function(token, err5) {
                   if (err4) console.error('add user 1 token err', err5);
-                  console.log('generated token', token)
+                  console.log('generated token', JSON.parse(JSON.stringify(token)));
                 })
               }
               resolve(user);
@@ -171,7 +180,7 @@ const setup = (cache, dispatcher) => {
           addChannelNote(chnl.id);
           // only can do this if we just created the userid 1
           if (privKey) {
-            console.log('need to create message 1!')
+            //console.log('need to create message 1!')
             addChannelMessage(privKey, chnl.id);
           }
         });
