c9bd80974afd99b96b1e5a0409259e55662546e4
Ryan Tharp <neuro@interx.net>
2020-08-17 05:35:21 +0000

    enableDebug, vars=>consts

--- overlay_server.js
+++ overlay_server.js
@@ -1,10 +1,19 @@
 if (process.env['config-file-path'] === undefined) {
   process.env['config-file-path'] = 'config.json';
 }
-const middlewares = require('./middlewares.js');
-var apps = require('./server/app.js');
+const loki_middlewares = require('./middlewares.js');
+const platform_middlewares = require('./server/middlewares.js');
+const apps = require('./server/app.js');
 
-apps.publicApp.use(middlewares.snodeOnionMiddleware);
+const enableDebug = false
+if (enableDebug) {
+  apps.publicApp.use(platform_middlewares.debugMiddleware);
+  // Express 4.x specific
+  // position it to spot 2
+  apps.publicApp._router.stack.splice(2, 0, apps.publicApp._router.stack.splice(apps.publicApp._router.stack.length - 1, 1)[0]);
+}
+
+apps.publicApp.use(loki_middlewares.snodeOnionMiddleware);
 // Express 4.x specific
 // position it to spot 2
 apps.publicApp._router.stack.splice(2, 0, apps.publicApp._router.stack.splice(apps.publicApp._router.stack.length - 1, 1)[0]);
